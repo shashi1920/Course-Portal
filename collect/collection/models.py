e5cecb6ec6aad74304cd1fd2ffe5c9dc1b785f9b
saurav kumar <saurav24007@gmail.com>
2015-06-29 17:32:16 +0530

    Update models.py

--- collect/collection/models.py
+++ collect/collection/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 
 class Profile(models.Model):
     user = models.OneToOneField(User)
-    level = models.IntegerField(max_length=1) #Level 0 is Professor, Level 1 is for HODs
+    level = models.BooleanField() #Level 0 is Professor, Level 1 is for HODs
 
     def __str__(self):              # __unicode__ on Python 2
         return self.user.username
@@ -18,7 +18,7 @@ class dept(models.Model):
 class programme(models.Model): #Btech/IDD/M.Tech/B.Pharm etc
     programme_code=models.CharField(max_length=5)
     programme_name=models.CharField(max_length=200)
-    duration=models.IntegerField(max_length=2)
+    duration=models.PositiveSmallIntegerField()
     branch=models.ForeignKey(dept,blank=False,null=False)
     def __str__(self):              # __unicode__ on Python 2
         return self.programme_code
@@ -42,7 +42,7 @@ class ApprovedCourseList(models.Model):
     course_code = models.CharField(max_length=10)#as IDD and B.Tech have same code so id is used
     course_name = models.CharField(max_length=150)
     syllabus = models.CharField(max_length=2500)
-    credit = models.IntegerField(max_length=2)
+    credit = models.PositiveSmallIntegerField()
     programme = models.ForeignKey(programme)
     level_0 = models.ForeignKey(Profile, null=True, blank=True, related_name='app_level_0_list')
     level_1 = models.ForeignKey(Profile, null=True, blank=True, related_name='app_level_1_list')
@@ -54,7 +54,7 @@ class ApprovedCourseList(models.Model):
 class ForeignCourseList(models.Model):
     course_code = models.CharField(max_length=10)
     course=models.ForeignKey(ApprovedCourseList)
-    credit = models.IntegerField()
+    credit = models.PositiveSmallIntegerField()
     programme = models.ForeignKey(programme)
     level_0 = models.ForeignKey(Profile, null=True, blank=True, related_name='for_level_0_list')
     level_1 = models.ForeignKey(Profile, null=True, blank=True, related_name='for_level_1_list')
@@ -70,9 +70,9 @@ class ProposedCourseList(models.Model):
     level_0 = models.ForeignKey(Profile, null=True, blank=True, related_name='pro_level_0_list')
     level_1 = models.ForeignKey(Profile, null=True, blank=True, related_name='pro_level_1_list')
     level_2 = models.ForeignKey(Profile, null=True, blank=True, related_name='pro_level_2_list')
-    level_0_sign = models.IntegerField(max_length=1)
-    level_1_sign = models.IntegerField(max_length=1)
-    level_2_sign = models.IntegerField(max_length=1)
+    level_0_sign = models.BooleanField()
+    level_1_sign = models.BooleanField()
+    level_2_sign = models.BooleanField()
     date_proposed= models.DateTimeField(auto_now=True)
     def __str__(self):              # __unicode__ on Python 2
         return self.course_code
@@ -81,14 +81,14 @@ class ApprovedCourseTeaching(models.Model):
     application_no = models.AutoField(primary_key=True)
     course_code = models.ForeignKey(ApprovedCourseList)
     prof_id1 = models.ForeignKey(Professor)
-    semester = models.IntegerField(max_length=2)
+    semester = models.PositiveSmallIntegerField()
     programme = models.ForeignKey(programme)
     course = models.CharField(max_length=10)
     date_added=models.DateTimeField(auto_now=True)
     prof_id2 = models.ForeignKey(Professor, null=True, blank=True, related_name='prof_id2')
     prof_id3 = models.ForeignKey(Professor, null=True, blank=True, related_name='prof_id3')
     prof_id4 = models.ForeignKey(Professor, null=True, blank=True, related_name='prof_id4')
-    elect_or_comp = models.IntegerField(max_length=1)
+    elect_or_comp = models.BooleanField()
     level_0 = models.ForeignKey(Profile, null=True, blank=True, related_name='app_level_0_teaching')
     level_1 = models.ForeignKey(Profile, null=True, blank=True, related_name='app_level_1_teaching')
     level_2 = models.ForeignKey(Profile, null=True, blank=True, related_name='app_level_2_teaching')
@@ -99,14 +99,14 @@ class ProposedCourseTeaching(models.Model):
     application_no = models.AutoField(primary_key=True)
     course_code = models.ForeignKey(ApprovedCourseList)
     prof_id = models.ForeignKey(Professor)
-    semester = models.IntegerField(max_length=2)
+    semester = models.PositiveSmallIntegerField()
     programme = models.ForeignKey(programme)
     course = models.CharField(max_length=10)
     level_0 = models.ForeignKey(Profile, null=True, blank=True, related_name='pro_level_0_teaching')
     level_1 = models.ForeignKey(Profile, null=True, blank=True, related_name='pro_level_1_teaching')
     level_2 = models.ForeignKey(Profile, null=True, blank=True, related_name='pro_level_2_teaching')
-    level_0_sign = models.IntegerField(max_length=1)
-    level_1_sign = models.IntegerField(max_length=1)
-    level_2_sign = models.IntegerField(max_length=1)
+    level_0_sign = models.BooleanField()
+    level_1_sign = models.BooleanField()
+    level_2_sign = models.BooleanField()
     def __str__(self):              # __unicode__ on Python 2
         return self.course_code
