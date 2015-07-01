0fcdeb1d12ca0e4585878b04a4047358a44bd034
shashi1920 <shashikant.sharma.cse12@iitbhu.ac.in>
2015-07-01 14:42:46 +0530

    integrated login logout

--- collect/collect/urls.py
+++ collect/collect/urls.py
@@ -8,9 +8,9 @@ urlpatterns = patterns('',
 
     url(r'^admin/', include(admin.site.urls)),
     url(r'^index/$', 'collection.views.index', name='index'),
-    url(r'^login/$', 'django.contrib.auth.views.login'),
+    url(r'^login/$', 'collection.views.login_user'),
     url(r'^logout/$', 'django.contrib.auth.views.logout'),
-    url(r'^programme/(?P<br>[A-Za-z]{2})/$', 'collection.views.programme_list'),
+    url(r'^programme/(?P<br>[A-Za-z]{2})/$', 'collection.views.programme_list', name='programme_list'),
     url(r'^programme/(?P<kr>[A-Za-z]{2})/(?P<pro>[A-Za-z]+)/(?P<sem>\d+)/$', 'collection.views.pro_course_list'),
     url(r'^programme/(?P<kr>[A-Za-z]{2})/(?P<pro>[A-Za-z]+)/(?P<sem>\d+)/submit/$', 'collection.views.add_teacher'),
 )
