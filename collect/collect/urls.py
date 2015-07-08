77f4f98d84b8725b1c782c737f843acd4e68cc1f
shashi1920 <shashikant.sharma.cse12@iitbhu.ac.in>
2015-07-09 03:41:12 +0530

    Print Feature and Foreign Course

--- collect/collect/urls.py
+++ collect/collect/urls.py
@@ -12,7 +12,13 @@ urlpatterns = patterns('',
     url(r'^logout/$', 'collection.views.logout_user', name='logout_user'),
     url(r'^programme/(?P<br>[A-Za-z]{2})/$', 'collection.views.programme_list', name='programme_list'),
     url(r'^programme/(?P<kr>[A-Za-z]{2})/(?P<pro>[A-Za-z]+)/(?P<sem>\d+)/$', 'collection.views.pro_course_list'),
+    url(r'^programme/(?P<kr>[A-Za-z]{2})/(?P<pro>[A-Za-z]+)/(?P<sem>\d+)/print/$', 'collection.views.pro_course_list_print'),
     url(r'^programme/(?P<kr>[A-Za-z]{2})/(?P<pro>[A-Za-z]+)/(?P<sem>\d+)/submit/$', 'collection.views.add_teacher'),
     url(r'^programme/(?P<br>[A-Za-z]{2})/(?P<pro>[A-Za-z]+)/(?P<sem>\d+)/delete/(?P<entry>\d+)/$', 'collection.views.delete_teacher'),
     url(r'^programme/(?P<br>[A-Za-z]{2})/activity_log/$', 'collection.views.activity_log_details'),
+    url(r'^programme/(?P<br>[A-Za-z]{2})/(?P<pro>[A-Za-z]+)/(?P<sem>\d+)/fr/$', 'collection.views.fr_course_list'),
+    url(r'^programme/(?P<br>[A-Za-z]{2})/(?P<pro>[A-Za-z]+)/(?P<sem>\d+)/fr/submit/$', 'collection.views.fr_submit'),
+    url(r'^programme/(?P<br>[A-Za-z]{2})/(?P<pro>[A-Za-z]+)/(?P<sem>\d+)/fr/delete/(?P<entry>\d+)/$', 'collection.views.fr_delete'),
+    url(r'^programme/(?P<br>[A-Za-z]{2})/(?P<pro>[A-Za-z]+)/(?P<sem>\d+)/fr/print/$', 'collection.views.fr_course_list_print'),
 )
+handler404='views.custom_404'
--- collect/collection/views.py
+++ collect/collection/views.py
@@ -13,6 +13,8 @@ from .models import ApprovedCourseTeaching
 from .models import ForeignCourseList
 from django.contrib import messages
 from .models import ActivityLog
+from django.db.models import Q
+from django.http import Http404
 def index(request):
     return render(request, 'collection/index.html')
 
@@ -24,8 +26,8 @@ def programme_list(request, br):
         return render(request,'registration/denied.html')
     pr=programme.objects.filter(branch=br)
     dr=dept.objects.get(dept_code=br)
-    activity_log=ActivityLog.objects.filter(USER=profile)[:5]
-    context={'programme_li' : pr,'deptm': dr,'activity_log':activity_log}
+    activity_log=ActivityLog.objects.filter(USER=profile).order_by('-date_time')[:5]
+    context={'programme_li' : pr,'deptm': dr,'activity_log':activity_log,'profile':profile}
     #return HttpResponse(br)
     return render(request,'collection/programme.html',context)
 
@@ -56,6 +58,33 @@ def pro_course_list(request, kr, pro,sem):
     url="/programme/"+kr+"/"+pro+"/"+sem+"/"
     context={'programme_li' : pr ,'course_li' : course_li,'professors':prof,'semester':sem,'teaching' : teaching,'url':url}
     return  render(request,'collection/course_list.html',context)
+def pro_course_list_print(request, kr, pro,sem):
+    if not request.user.is_authenticated():
+        return render(request,'registration/denied.html')
+    profile = Profile.objects.filter(user=request.user)
+    if len(dept.objects.filter(head=profile))==0 or str(dept.objects.filter(head=profile)[0].dept_code) != kr:
+        return render(request,'registration/denied.html')
+    pr=programme.objects.get(branch=kr,programme_code=pro)
+    #msg=br+PR
+    #return HttpResponse(pro)
+    teaching=ApprovedCourseTeaching.objects.filter(programme=pr,semester=sem)
+    courses=ApprovedCourseList.objects.filter(programme=pr,semester=sem)
+    global flag
+    flag=1
+    course_li=[]
+    if courses:
+        for cr in courses:
+            if teaching:
+                for teach in teaching:
+                    if teach.course_code.course_code==cr.course_code:
+                        flag=0
+            if(flag==1):
+                course_li.append(cr)
+            flag=1
+    prof=Professor.objects.all().order_by('prof_name')
+    url="/programme/"+kr+"/"+pro+"/"+sem+"/"
+    context={'programme_li' : pr ,'course_li' : course_li,'professors':prof,'semester':sem,'teaching' : teaching,'url':url}
+    return  render(request,'collection/print.html',context)
 
 def login_user(request):
     state = ""
@@ -235,6 +264,93 @@ def activity_log_details(request,br):
     activity_log=ActivityLog.objects.filter(USER=profile)
     context={'activity_log' : activity_log}
     return render(request,'collection/activity_log.html',context)
+def custom404(request):
+    return render(request,'collection/404.html')
+
+def fr_course_list_print(request,br, pro,sem):
+    if not request.user.is_authenticated():
+        return render(request,'registration/denied.html')
+    profile = Profile.objects.get(user=request.user)
+    if len(dept.objects.filter(head=profile))==0 or str(dept.objects.filter(head=profile)[0].dept_code) != br:
+        return render(request,'registration/denied.html')
+
+    prog=programme.objects.filter(~Q(branch = br))
+    pr=programme.objects.get(branch=br,programme_code=pro)
+    courses=ApprovedCourseList.objects.filter(programme=pr,semester=sem)
+    course_offered_from=ForeignCourseList.objects.filter(course=courses)
+    context={'programme':prog,'courses':courses,'course_offered_from':course_offered_from,'semester':sem}
+    return render(request,'collection/foreign_print.html',context)
+
+def fr_course_list(request,br, pro,sem):
+    if not request.user.is_authenticated():
+        return render(request,'registration/denied.html')
+    profile = Profile.objects.get(user=request.user)
+    if len(dept.objects.filter(head=profile))==0 or str(dept.objects.filter(head=profile)[0].dept_code) != br:
+        return render(request,'registration/denied.html')
+
+    prog=programme.objects.filter(~Q(branch = br))
+    pr=programme.objects.get(branch=br,programme_code=pro)
+    courses=ApprovedCourseList.objects.filter(programme=pr,semester=sem)
+    course_offered_from=ForeignCourseList.objects.filter(course=courses)
+    context={'programme':prog,'courses':courses,'course_offered_from':course_offered_from,'semester':sem}
+    return render(request,'collection/404.html',context)
+
+def fr_submit(request,br,pro,sem):
+    if not request.user.is_authenticated():
+        return render(request,'registration/denied.html')
+    profile = Profile.objects.get(user=request.user)
+    if len(dept.objects.filter(head=profile))==0 or str(dept.objects.filter(head=profile)[0].dept_code) != br:
+        return render(request,'registration/denied.html')
+    for i in "54321":
+        fr_pr='fr_program'+i
+        frpr=request.POST.get(fr_pr)
+        fr_cr='fr_course'+i
+        frcr=request.POST.get(fr_cr)
+        credit='credits'+i
+        credit_get=request.POST.get(credit)
+        seme='sem'+i
+        SEM=request.POST.get(seme)
+        ltp="LTP"+i
+        LTP=request.POST.get(ltp)
+        global L
+        global T
+        global P
+
+        if LTP:
+            LTP=int(LTP)
+            P = LTP%10
+            LTP = LTP/10
+            T = LTP%10
+            LTP = LTP/10
+            L = LTP%10
+
+        if frpr and frcr:
+            pr=programme.objects.get(pk=frpr)
+            cr=ApprovedCourseList.objects.get(pk=frcr)
+            COURSE_CODE=cr.course_code+"A"
+            if not SEM and not credit_get:
+                msg="Sorry, Credit and Semester is also required for "+cr.course_code
+                messages.error(request,msg)
+            else:
+                if(int(SEM)>(2*pr.duration)):
+                    msg="Sorry, Semester count can't be greater than maximum duration for  "+cr.course_code
+                    messages.error(request,msg)
+                else:
+                    ins=ForeignCourseList(programme=pr,course_code=COURSE_CODE,course=cr,credit=credit_get,semester=SEM,l=L,t=T,p=P)
+                    ins.save()
+                    msg="Successfully Offered " + cr.course_code + " " + cr.course_name +" to "+pr.programme_name+"("+pr.programme_code+") "+pr.branch.dept_name
+                    messages.success(request,msg)
+                    p=ActivityLog(USER=profile,log=msg)
+                    p.save()
+
+    url="/programme/"+br+"/"+pro+"/"+sem+'/fr'
+    return HttpResponseRedirect(url)
+def fr_delete(request,br, pro,sem,entry):
+    if not request.user.is_authenticated():
+        return render(request,'registration/denied.html')
+    profile = Profile.objects.get(user=request.user)
+    if len(dept.objects.filter(head=profile))==0 or str(dept.objects.filter(head=profile)[0].dept_code) != br:
+        return render(request,'registration/denied.html')
 
 
 # Create your views here.
